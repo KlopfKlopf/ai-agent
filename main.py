e5d617ad3a993bd9f8f2900071930056ff942c05
KlopfKlopf9 <linus93@live.de>
2025-07-13 12:06:09 +0200

    Add prompt through CLI

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+
+    load_dotenv()
+    args = sys.argv[1:]
+    if not args:
+        print("Error no prompt provided. Please retry and provide a prompt.")
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    candidate_tokens = response.usage_metadata.candidates_token_count
+    
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {candidate_tokens}")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
